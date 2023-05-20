35dab26406c487dd88a048d9ec754965e07c7a9d
TkNazarii <tknazkooo@gmail.com>
2023-05-20 20:55:51 +0300

    asdd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,85 @@
-import {Routes, Route, NavLink, Navigate } from "react-router-dom";
+import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
-import Home from "./01_Home";
-import Movies from "./02_Movies";
-import MovieDetails from "./03_MovieDetails";
-import Cast from "./04_Cast";
-import Reviews from "./05_Reviews/";
+import Home from './01_Home';
+// import Movies from "./02_Movies";
+// import MovieDetails from './03_MovieDetails';
+// import Cast from './04_Cast';
+// import Reviews from './05_Reviews/';
 
 import css from './App.module.css';
 
+const Movies = lazy(() => import('components/02_Movies'));
+const MovieDetails = lazy(() => import('components/03_MovieDetails'));
+const Cast = lazy(() => import('components/04_Cast'));
+const Reviews = lazy(() => import('components/05_Reviews'));
+
 export const App = () => {
-	
-	const routesName = [
-		{
-			path: '/',
-			label: 'Home',
-		},
-		{
-			path: '/movies',
-			label: 'Movies',
-		}
-	]
-	
-	return (
-		<>
-		<header className={css['app']}>
-			<nav>
-			{routesName.map((el) => {
-					return <NavLink to={el.path} key={el.path}>{el.label}</NavLink>
-				})}
-			</nav>
-		</header>
+  const routesName = [
+    {
+      path: '/',
+      label: 'Home',
+    },
+    {
+      path: '/movies',
+      label: 'Movies',
+    },
+  ];
 
-		<section className={css['app']}>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/movies" element={<Movies />} />
-				<Route path="/movies/:movieId" element={ <MovieDetails />}>
-					<Route path="cast" element={<Cast />} />
-					<Route path="reviews" element={<Reviews />} />
-				</Route>
-				<Route path="*" element={<Navigate to='/'/>} />
-			</Routes>
-		</section>
+  return (
+    <>
+      <header className={css['app']}>
+        <nav>
+          {routesName.map(el => {
+            return (
+              <NavLink to={el.path} key={el.path}>
+                {el.label}
+              </NavLink>
+            );
+          })}
+        </nav>
+      </header>
 
+      <section className={css['app']}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/movies"
+            element={
+              <Suspense fallback={<p>Зачекайте</p>}>
+                <Movies />
+              </Suspense>
+            }
+          />
+
+          <Route
+            path="/movies/:movieId"
+            element={
+              <Suspense fallback={<p>Зачекайте</p>}>
+                <MovieDetails />
+              </Suspense>
+            }
+          >
+            <Route
+              path="cast"
+              element={
+                <Suspense fallback={<p>Зачекайте</p>}>
+                  <Cast />
+                </Suspense>
+              }
+            />
+            <Route
+              path="reviews"
+              element={
+                <Suspense fallback={<p>Зачекайте</p>}>
+                  <Reviews />
+                </Suspense>
+              }
+            />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </section>
     </>
   );
 };
